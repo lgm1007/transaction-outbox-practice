3dbf68373d3ea42e286695291d4c41c863bc063f
lgm1007 <flxl654@gmail.com>
2024-09-19 11:14:44 +0900

    feat: init repository

---
+++ src/main/kotlin/com/example/transactionoutboxpractice/TransactionOutboxPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.transactionoutboxpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransactionOutboxPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TransactionOutboxPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/transactionoutboxpractice/TransactionOutboxPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.transactionoutboxpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransactionOutboxPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
