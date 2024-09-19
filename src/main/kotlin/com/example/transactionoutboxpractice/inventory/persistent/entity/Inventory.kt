4f6b1af1dd07b8330e6b0edde838bddad5901cf7
lgm1007 <flxl654@gmail.com>
2024-09-19 11:55:33 +0900

    feat: Inventory 영속성 Entity 추가

---
+++ src/main/kotlin/com/example/transactionoutboxpractice/inventory/persistent/entity/Inventory.kt
@@ -0,0 +1,16 @@
+package com.example.transactionoutboxpractice.inventory.persistent.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Inventory(
+    val item: String,
+    var count: Int,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+}
